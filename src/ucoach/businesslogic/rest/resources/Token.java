0a69b3b931f23fa6c8c6558f60f8f65fb489c55d
ferraricharles <charles.cjf@gmail.com>
2016-02-04 18:31:21 +0100

    Responses bugs fixing

--- src/ucoach/businesslogic/rest/resources/Token.java
+++ src/ucoach/businesslogic/rest/resources/Token.java
@@ -18,28 +18,45 @@ import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Request;
 import javax.ws.rs.core.Response;
-
+import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.core.UriInfo;
 import javax.xml.ws.WebServiceContext;
 
 import javax.annotation.Resource;
 
 @Path("/token/{token}")
 public class Token {
-
+	@Context
+	UriInfo uriInfo;
+	
+	
+	/**
+	 * Method to return the user according to its token
+	 * Verify client authorization
+	 * Return the user code if the token is valid
+	 * 
+	 */	
 	@GET
 	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
-    public long getPerson(@Context HttpHeaders headers, @PathParam("token") String token) throws Exception {
-		
+    public Response getPerson(@Context HttpHeaders headers, @PathParam("token") String token) throws Exception {		
 		Authorization.validateRequest(headers);
+		Response res;
 		
-		if(Authorization.validateRequest(headers)){
-			Long l =  Authenticator.authenticate(token);
-			return l;
-		}else{
-			Response r = Response.noContent().build();
-			return 0;
+		if(! Authorization.validateRequest(headers)){
+			Response r = Response.status(401).build();
+			return r;
 		}
 		
-        
+		try{
+			Long l =  Authenticator.authenticate(token);
+			if(l!=0){
+				res = Response.status(Status.OK).entity(l).location(uriInfo.getAbsolutePath()).build();
+			}
+			res = Response.noContent().build();				
+		}catch(Exception e){
+			res = Response.serverError().build();
+		}
+			
+		return res;		      
     }
 }
